471d23fc8ecfc64dfa5029a500badc75270bcc2f
GL150 <de.micheli.gianluca@gmail.com>
2021-09-01 18:26:39 -0300

    Create Login Page

---
+++ Desktop_App/TGS/Login.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace TGS {
+    partial class Login {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Login));
+            this.img_Logo = new System.Windows.Forms.PictureBox();
+            this.lbl_TitleLogin = new System.Windows.Forms.Label();
+            this.lbl_User = new System.Windows.Forms.Label();
+            this.txt_User = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.lbl_Password = new System.Windows.Forms.Label();
+            this.btn_Login = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // img_Logo
+            // 
+            this.img_Logo.BackColor = System.Drawing.Color.Transparent;
+            this.img_Logo.Image = ((System.Drawing.Image)(resources.GetObject("img_Logo.Image")));
+            this.img_Logo.Location = new System.Drawing.Point(580, 87);
+            this.img_Logo.Name = "img_Logo";
+            this.img_Logo.Size = new System.Drawing.Size(120, 120);
+            this.img_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_Logo.TabIndex = 0;
+            this.img_Logo.TabStop = false;
+            // 
+            // lbl_TitleLogin
+            // 
+            this.lbl_TitleLogin.AutoSize = true;
+            this.lbl_TitleLogin.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_TitleLogin.Font = new System.Drawing.Font("Consolas", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_TitleLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(88)))), ((int)(((byte)(145)))));
+            this.lbl_TitleLogin.Location = new System.Drawing.Point(476, 229);
+            this.lbl_TitleLogin.Name = "lbl_TitleLogin";
+            this.lbl_TitleLogin.Size = new System.Drawing.Size(328, 47);
+            this.lbl_TitleLogin.TabIndex = 1;
+            this.lbl_TitleLogin.Text = "Seja Bem-Vindo";
+            this.lbl_TitleLogin.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lbl_User
+            // 
+            this.lbl_User.AutoSize = true;
+            this.lbl_User.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_User.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_User.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(88)))), ((int)(((byte)(145)))));
+            this.lbl_User.Location = new System.Drawing.Point(440, 336);
+            this.lbl_User.Name = "lbl_User";
+            this.lbl_User.Size = new System.Drawing.Size(143, 37);
+            this.lbl_User.TabIndex = 2;
+            this.lbl_User.Text = "Usuário";
+            // 
+            // txt_User
+            // 
+            this.txt_User.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_User.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_User.ForeColor = System.Drawing.Color.Black;
+            this.txt_User.Location = new System.Drawing.Point(440, 379);
+            this.txt_User.Name = "txt_User";
+            this.txt_User.Size = new System.Drawing.Size(400, 36);
+            this.txt_User.TabIndex = 3;
+            this.txt_User.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.BackColor = System.Drawing.SystemColors.Window;
+            this.txt_Password.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txt_Password.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.txt_Password.ForeColor = System.Drawing.Color.Black;
+            this.txt_Password.Location = new System.Drawing.Point(440, 483);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.PasswordChar = '*';
+            this.txt_Password.Size = new System.Drawing.Size(400, 36);
+            this.txt_Password.TabIndex = 5;
+            this.txt_Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_Password
+            // 
+            this.lbl_Password.AutoSize = true;
+            this.lbl_Password.BackColor = System.Drawing.Color.Transparent;
+            this.lbl_Password.Font = new System.Drawing.Font("Consolas", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lbl_Password.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(88)))), ((int)(((byte)(145)))));
+            this.lbl_Password.Location = new System.Drawing.Point(440, 440);
+            this.lbl_Password.Name = "lbl_Password";
+            this.lbl_Password.Size = new System.Drawing.Size(107, 37);
+            this.lbl_Password.TabIndex = 4;
+            this.lbl_Password.Text = "Senha";
+            // 
+            // btn_Login
+            // 
+            this.btn_Login.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(130)))), ((int)(((byte)(219)))));
+            this.btn_Login.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(61)))), ((int)(((byte)(130)))), ((int)(((byte)(219)))));
+            this.btn_Login.FlatAppearance.BorderSize = 0;
+            this.btn_Login.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(24)))), ((int)(((byte)(74)))), ((int)(((byte)(137)))));
+            this.btn_Login.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(88)))), ((int)(((byte)(145)))));
+            this.btn_Login.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Login.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Login.ForeColor = System.Drawing.Color.White;
+            this.btn_Login.Location = new System.Drawing.Point(746, 556);
+            this.btn_Login.Name = "btn_Login";
+            this.btn_Login.Size = new System.Drawing.Size(94, 42);
+            this.btn_Login.TabIndex = 6;
+            this.btn_Login.Text = "Entrar";
+            this.btn_Login.UseVisualStyleBackColor = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.btn_Login);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.lbl_Password);
+            this.Controls.Add(this.txt_User);
+            this.Controls.Add(this.lbl_User);
+            this.Controls.Add(this.lbl_TitleLogin);
+            this.Controls.Add(this.img_Logo);
+            this.DoubleBuffered = true;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Name = "Login";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Login_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.img_Logo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox img_Logo;
+        private System.Windows.Forms.Label lbl_TitleLogin;
+        private System.Windows.Forms.Label lbl_User;
+        private System.Windows.Forms.TextBox txt_User;
+        private System.Windows.Forms.TextBox txt_Password;
+        private System.Windows.Forms.Label lbl_Password;
+        private System.Windows.Forms.Button btn_Login;
+    }
+}
+
---
+++ Desktop_App/TGS/Login.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TGS {
+    public partial class Login : Form {
+        public Login() {
+            InitializeComponent();
+        }
+
+        private void Login_FormClosing(object sender, FormClosingEventArgs e) {
+            if (MessageBox.Show("Deseja realmente sair?", "Atenção", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                Application.ExitThread();
+            } else {
+                e.Cancel = true;
+            }
+        }
+    }
+}

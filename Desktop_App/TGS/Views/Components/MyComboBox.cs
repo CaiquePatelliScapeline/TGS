3e27cf3527c83ff919b24625ba688647e644a939
GL150 <de.micheli.gianluca@gmail.com>
2021-10-07 20:53:11 -0300

    Update MyComboBox
    Co-Authored-By: Caique <56651727+CaiquePatelliScapeline@users.noreply.github.com>

--- Desktop_App/TGS/Views/Components/MyComboBox.cs
+++ Desktop_App/TGS/Views/Components/MyComboBox.cs
@@ -228,39 +228,41 @@ namespace TGS.Views.Components {
             this.SuspendLayout();
 
             // ComboBox: Dropdown List
-            cmbList.BackColor = ListBackColor;
+            cmbList.BackColor = listBackColor;
             cmbList.Font = new Font(this.Font.Name, 10F);
-            cmbList.ForeColor = ListTextColor;
-            cmbList.SelectedIndexChanged += new EventHandler(ComboBox_SelectedIndexChanged); // Default Event
-            cmbList.TextChanged += new EventHandler(ComboBox_TextChanged);
+            cmbList.ForeColor = listTextColor;
+            cmbList.SelectedIndexChanged += new EventHandler(ComboBox_SelectedIndexChanged); // Default event
+            cmbList.TextChanged += new EventHandler(ComboBox_TextChanged); // Refresh text
 
             // Button: Icon
             btnIcon.Dock = DockStyle.Right;
             btnIcon.FlatStyle = FlatStyle.Flat;
             btnIcon.FlatAppearance.BorderSize = 0;
-            btnIcon.BackColor = BackColor;
+            btnIcon.BackColor = backColor;
             btnIcon.Size = new Size(30, 30);
             btnIcon.Cursor = Cursors.Hand;
-            btnIcon.Click += new EventHandler(Icon_Click); // Open Dropdown List
-            btnIcon.Paint += new PaintEventHandler(Icon_Paint);
+            btnIcon.Click += new EventHandler(Icon_Click); // Open dropdown list
+            btnIcon.Paint += new PaintEventHandler(Icon_Paint); // Draw icon
 
             // Label: Text
             lblText.Dock = DockStyle.Fill;
             lblText.AutoSize = false;
-            lblText.BackColor = BackColor;
+            lblText.BackColor = backColor;
             lblText.TextAlign = ContentAlignment.MiddleLeft;
             lblText.Padding = new Padding(8, 0, 0, 0);
             lblText.Font = new Font(this.Font.Name, 10F);
-            lblText.Click += new EventHandler(Surface_Click); // Select Combo Box
+            lblText.Click += new EventHandler(Surface_Click); //Select combo box
 
             // User Control
-            this.Controls.Add(lblText); // 2
-            this.Controls.Add(btnIcon); // 1
-            this.Controls.Add(cmbList); // 0
+            this.Controls.Add(lblText); //2
+            this.Controls.Add(btnIcon); //1
+            this.Controls.Add(cmbList); //0
             this.MinimumSize = new Size(200, 30);
+            this.Size = new Size(200, 30);
             this.ForeColor = Color.DimGray;
-            this.Padding = new Padding(BorderSize);
-            base.BackColor = BorderColor;
+            this.Padding = new Padding(borderSize); //Border Size
+            this.Font = new Font(this.Font.Name, 10F);
+            base.BackColor = borderColor; //Border Color
             this.ResumeLayout();
             AdjustComboBoxDimensions();
         }
@@ -275,49 +277,49 @@ namespace TGS.Views.Components {
         }
 
         // Event Methods
-        private void Surface_Click(object sender, EventArgs e) {
-            // Select Combo Box
-            cmbList.Select();
-            if (cmbList.DropDownStyle == ComboBoxStyle.DropDownList) {
-                cmbList.DroppedDown = true; // Open Dropdown List
-            }
-        }
-
-        private void Icon_Paint(object sender, PaintEventArgs e) {
-            // Fields
-            int iconWidth = 14;
-            int iconHeight = 6;
-            var rectIcon = new Rectangle((btnIcon.Width - iconWidth) / 2, (btnIcon.Height - iconHeight) / 2, iconWidth, iconHeight);
-            Graphics graph = e.Graphics;
-
-            // Draw Arrow Down Icon
-            using (GraphicsPath path = new GraphicsPath())
-            using (Pen pen = new Pen(IconColor, 2)) {
-                graph.SmoothingMode = SmoothingMode.AntiAlias;
-                path.AddLine(rectIcon.X, rectIcon.Y, rectIcon.X + (iconWidth / 2), rectIcon.Bottom);
-                path.AddLine(rectIcon.X + (iconWidth / 2), rectIcon.Bottom, rectIcon.Right, rectIcon.Y);
-                graph.DrawPath(pen, path);
-            }
+        // Default Event
+        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e) {
+            if (OnSelectedIndexChanged != null)
+            OnSelectedIndexChanged.Invoke(sender, e);
+            // Refresh text
+            lblText.Text = cmbList.Text;
         }
 
         private void Icon_Click(object sender, EventArgs e) {
-            // Open Dropdown List
+            // Open dropdown list
             cmbList.Select();
             cmbList.DroppedDown = true;
         }
 
+        private void Surface_Click(object sender, EventArgs e) {
+            //Attach label click to user control click
+            this.OnClick(e);
+            //Select combo box
+            cmbList.Select();
+            if (cmbList.DropDownStyle == ComboBoxStyle.DropDownList)
+                cmbList.DroppedDown = true; // Open dropdown list
+        }
+
         private void ComboBox_TextChanged(object sender, EventArgs e) {
-            // Refresh Text
+            //Refresh text
             lblText.Text = cmbList.Text;
         }
 
-        // Default Event
-        private void ComboBox_SelectedIndexChanged(object sender, EventArgs e) {
-            if (OnSelectedIndexChanged != null) {
-                OnSelectedIndexChanged.Invoke(sender, e);
+        private void Icon_Paint(object sender, PaintEventArgs e) {
+            // Fields
+            int iconWidht = 14;
+            int iconHeight = 6;
+            var rectIcon = new Rectangle((btnIcon.Width - iconWidht) / 2, (btnIcon.Height - iconHeight) / 2, iconWidht, iconHeight);
+            Graphics graph = e.Graphics;
+            // Draw arrow down icon
+            using (GraphicsPath path = new GraphicsPath())
+            using (Pen pen = new Pen(iconColor, 2))
+            {
+                graph.SmoothingMode = SmoothingMode.AntiAlias;
+                path.AddLine(rectIcon.X, rectIcon.Y, rectIcon.X + (iconWidht / 2), rectIcon.Bottom);
+                path.AddLine(rectIcon.X + (iconWidht / 2), rectIcon.Bottom, rectIcon.Right, rectIcon.Y);
+                graph.DrawPath(pen, path);
             }
-            // Refresh Text
-            lblText.Text = cmbList.Text;
         }
     }
 }
